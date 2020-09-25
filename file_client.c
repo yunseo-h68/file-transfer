e6250ae64889fc84fa2d5534dc83c96b56fabb8f
Yunseo Hwang <yunseo.h68@gmail.com>
2020-09-25 22:43:27 +0900

    Doc : 상세한 주석 추가

--- file_client.c
+++ file_client.c
@@ -48,16 +48,24 @@ int main(int argc, char* argv[])
 	err_handling(err_chk, -1, "connect() error");
 	
 	while(1) {
+		// File 이름을 입력받아 서버로 전송
+		// 만약 입력 받은 값이 'quit'이면 서버와 클라이언트 모두 종료
 		putchar('>');
 		scanf("%s", buf);
 		if (!strcmp(buf, "quit"))
 			break;
-
 		write(sock, buf, BUF_SIZE);
 		
+		// 서버로부터 받은 파일을 저장할 경로를 path 변수에 저장.
+		// CLIENT_DIR에 설정된 경로와 위에서 입력받은 파일명을 더함.
+		// ex) 'client/' + 'test.txt' => 'client/test.txt'
 		strcpy(path, CLIENT_DIR);
 		strcat(path, buf);
 		
+		// 입력한 파일이 서버에 존재하는지 확인.
+		// 만약 서버로부터 'FOUND'가 전송되었으면 서버에 파일이 존재.
+		// 만약 'NOT FOUND' 혹은 이외의 값이 전송되었으면 파일이 존재하지 않음.
+		// 파일이 없으면 다시 파일명을 입력받는 부분으로 돌아감.
 		len = read(sock, buf, BUF_SIZE);
 		err_handling(len, -1, "read() error");
 		if (strcmp(buf, "FOUND")) {
@@ -66,6 +74,9 @@ int main(int argc, char* argv[])
 		}
 		puts("Fild Found ...");
 
+		// 파일의 정보를 서버로부터 받음.
+		// file size와 file mode를 받으며
+		// 위에서 설정한 파일 경로(path)와 file mode로 파일을 생성함.
 		len = read(sock, &file_size, sizeof(off_t));
 		err_handling(len, -1, "read() error");
 		len = read(sock, &file_mode, sizeof(mode_t));
@@ -74,6 +85,9 @@ int main(int argc, char* argv[])
 		printf("... File Mode : %X\n", file_mode);
 		fd = creat(path, file_mode);
 
+		// 만약 파일 생성에 실패할 경우('client' 디렉토리가 없을 경우 등)
+		// 서버에 'no'를 보내고 다시 파일 이름을 입력받는 부분으로 돌아감.
+		// 서버는 'no'를 수신하면 파일을 전송하지 않고 다시 대기 상태.
 		if (fd == -1) {
 			fputs("Failed to create file : ", stdout);
 			puts(path);
@@ -81,24 +95,32 @@ int main(int argc, char* argv[])
 			write(sock, buf, BUF_SIZE);
 			continue;
 		}
+		// 파일이 정상적으로 생성되었으면 'ok'를 서버에 보내고
+		// 파일 다운로드를 시작함.
 		fputs("File created : ", stdout);
 		puts(path);
 		strcpy(buf, "ok");
 		write(sock, buf, BUF_SIZE);
-
+		
+		// 서버로부터 BUF_SIZE만큼 데이터를 읽어 읽은 byte 수(len)만큼
+		// 위에서 생성한 파일에 작성함.
 		puts("Start download ...");
-		gettimeofday(&start_time, NULL);
+		gettimeofday(&start_time, NULL); // 시작하기 전 시간을 start_time에 저장
 		while ((len = read(sock, buf, BUF_SIZE)) > 0) {
 			write(fd, buf, len);
+			// bytes_count에 현재까지 읽은 총 byte 수를 저장하고
+			// 만약 bytes_count가 file_size보다 크거나 같다면 데이터 수신을 종료함.
 			bytes_count += len;
 			if (bytes_count >= file_size) break;
 		}
-		gettimeofday(&end_time, NULL);
+		gettimeofday(&end_time, NULL); // 끝난 시간을 end_time에 저장
 
+		// 끝난 시간에서 시작 시간을 빼서 수행 시간(work_time)을 계산함.
 		work_time = (double)(end_time.tv_sec)
 			+ (double)(end_time.tv_usec)/1000000.0
 			- (double)(start_time.tv_sec)
 			- (double)(start_time.tv_usec)/1000000.0;
+		// 전송 받은 byte 수, 수행 시간, 초당 수신 bytes 수 등의 정보를 출력함.
 		printf("... File Size : %ld\n", bytes_count);
 		printf("... Time Spent : %.3lf\n", work_time);
 		printf("... %.3lf b/s\n", (double)bytes_count/work_time);
@@ -106,10 +128,12 @@ int main(int argc, char* argv[])
 		printf("... %.3lf mb/s\n", (double)(bytes_count/1024.0f/1024.0f)/work_time);
 		puts("Download complete");
 
+		// 생성한 파일을 닫고 변수를 초기화함.
 		close(fd);
 		len = 0;
 		bytes_count = 0;
 	}
+	// 서버에 'quit'를 전송하여 서버와 클라이언트 모두 종료되도록 함.
 	strcpy(buf, "quit");
 	write(sock, buf, BUF_SIZE);
 	close(sock);
@@ -118,6 +142,8 @@ int main(int argc, char* argv[])
 
 int err_handling(int err_chk, const int err_num, const char* message)
 {
+	// 만약 err_chk와 err_num이 같다면 에러를 출력하고 종료.
+	// 아니라면 err_chk 값을 그대로 반환함.
 	if (err_chk != err_num) {
 		return err_chk;
 	}
--- file_server.c
+++ file_server.c
@@ -54,20 +54,30 @@ int main(int argc, char* argv[])
 	err_handling(clnt_sock, -1, "accept() error");
 
 	while(1) {
+		// 클라이언트로부터 파일의 이름을 받음
+		// 만약 파일의 이름이 'quit'이면 서버와 클라이언트 모두 종료
 		len = read(clnt_sock, buf, BUF_SIZE);
 		err_handling(len, -1, "read() error");
 		if (!strlen(buf)) 
 			continue;
 		if(!strcmp(buf, "quit"))
 			break;
-
+		
+		// 요청 받은 파일의 경로 출력
 		putchar('\n');
 		fputs("Requested File Path : ", stdout);
 		puts(buf);
 		
-		//
+		// 파일을 찾을 경로를 path 변수에 저장
+		// 기본적으로 SERVER_DIR 아래에서 파일을 찾으므로
+		// path를 SERVER_DIR + 파일 경로로 설정함.
+		// ex) 'server/' + 'test.txt' => 'server/test.txt'
 		strcpy(path, SERVER_DIR);
 		strcat(path, buf);
+
+		// 요청 받은 파일을 여는 데 실패하면(파일이 없는 경우 등)
+		// 'NOT FOUND'를 클라이언트에 전송하고 파일이름 수신 대기로 돌아감.
+		// 파일을 여는 데 성공하면 'FOUND'를 클라이언트에 전송
 		fd = open(path, O_RDONLY);
 		if (fd == -1) {
 			fputs("... NOT FOUND : ", stdout);
@@ -80,17 +90,21 @@ int main(int argc, char* argv[])
 		strcpy(buf, "FOUND");
 		write(clnt_sock, buf, BUF_SIZE);
 
+		// 요청 받은 파일의 정보(file size, file mode)를 읽어들여
+		// 클라이언트로 전송함
 		err_chk = fstat(fd, &file_stat);
 		err_handling(err_chk, -1, "fstat() error");
 		file_size = file_stat.st_size;
 		file_mode = file_stat.st_mode;
-
 		puts("File Found");
 		printf("- File Size : %ld\n", file_size);
 		printf("- File Mode : %X\n", file_mode);
 		write(clnt_sock, &file_size, sizeof(off_t));
 		write(clnt_sock, &file_mode, sizeof(mode_t));
-
+		
+		// 클라이언트로부터 'ok' 신호가 오기를 대기함
+		// 만약 'ok'가 아니라 다른 신호('no' 등)이 온다면
+		// 파일 요청에 실패한 것으로 보고 파일이름 수신 대기로 돌아감.
 		while((len = read(clnt_sock, buf, BUF_SIZE))&&!strlen(buf));
 		err_handling(len, -1, "read() error");
 		if (strcmp(buf, "ok")) {
@@ -98,20 +112,30 @@ int main(int argc, char* argv[])
 			puts("File request failed");
 			continue;
 		}
-
+		
+		// 만약 'ok' 신호가 왔다면
+		// 클라이언트가 파일 수신 준비 상태인 것으로 보고
+		// 파일 전송을 시작함.
+		// 요청 받은 파일로부터 BUF_SIZE만큼 데이터를 읽어
+		// 읽은 byte 수만큼 그대로 클라이언트에게 전송함
 		puts("Start File Transfer ... ");
 		while ((len = read(fd, buf, BUF_SIZE)) > 0) {
 			write(clnt_sock, buf, len);
 		}
 		puts("File Transfer Completed");
+
+		// 생성한 파일을 닫음
 		close(fd);
 	}
+	// 서버와 클라이언트 모두 종료
 	close(clnt_sock);
 	close(serv_sock);
 }
 
 int err_handling(int err_chk, const int err_num, const char* message)
 {
+	// 만약 err_chk와 err_num이 같다면 에러를 출력하고 종료
+	// 아니라면 err_chk 값을 그대로 반환함
 	if (err_chk != err_num) {
 		return err_chk;
 	}
